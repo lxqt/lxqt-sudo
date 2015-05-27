a6cc2ef65e4722922b0a54f107ce5025eab21729
Palo Kisa <palo.kisa@gmail.com>
2015-05-27 08:29:48 +0200

    lxqt-sudo: added sk translation

---
+++ translations/lxqt-sudo_sk.ts
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>Communication</name>
+    <message>
+        <location filename="../communication.cpp" line="85"/>
+        <source>%1: Communication - unable to create shared memory(%2B), </source>
+        <translation>%1: Communication - vytváranie zdieľanej pamäte(%2B) zlyhalo, </translation>
+    </message>
+    <message>
+        <location filename="../communication.cpp" line="183"/>
+        <source>%1: Communication - password is too long(%2) for aquired shared memory(%3), has to be cut-off...
+</source>
+        <translation>%1: Communication - heslo je príliš dlhé(%2) pre použitú zdieľanú pamäť(%3), musí byť orezané...
+</translation>
+    </message>
+</context>
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../passworddialog.ui" line="20"/>
+        <location filename="../build/ui_passworddialog.h" line="129"/>
+        <source>LXQt sudo</source>
+        <translation>LXQt sudo</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="77"/>
+        <location filename="../build/ui_passworddialog.h" line="134"/>
+        <source>Command:</source>
+        <translation>Príkaz:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="84"/>
+        <location filename="../build/ui_passworddialog.h" line="135"/>
+        <source>Password:</source>
+        <translation>Heslo:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="43"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
+Please enter your password.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; vyžaduje práva administrátora.
+Prosím, zadajte svoje heslo.</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="53"/>
+        <source>Attempt #%1</source>
+        <translation>Pokus č. %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="57"/>
+        <source>Usage: %1 command [arguments...]
+
+GUI frontend for %2
+
+Arguments:
+  command        Command to run.
+  arguments      Optional arguments for command.
+
+</source>
+        <translation>Použitie: %1 command [arguments...]
+
+GUI frontend pre %2
+
+Parametre:
+  command        Príkaz na spustenie.
+  arguments      Parametre príkazu.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="74"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1: žiaden príkaz na spustenie!</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="145"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation>Dcérsky &apos;%1&apos; process zlyhal!
+%2</translation>
+    </message>
+</context>
+</TS>
