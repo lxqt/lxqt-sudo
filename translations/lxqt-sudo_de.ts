a938e7ca106c46a3288cd778f679739f9267b996
Peter Mattern <matternp@arcor.de>
2015-06-19 16:56:09 +0200

    German translation

---
+++ translations/lxqt-sudo_de.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../passworddialog.ui" line="20"/>
+        <location filename="../build/ui_passworddialog.h" line="129"/>
+        <source>LXQt sudo</source>
+        <translation>LXQt sudo</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="77"/>
+        <location filename="../build/ui_passworddialog.h" line="134"/>
+        <source>Command:</source>
+        <translation>Befehl:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="84"/>
+        <location filename="../build/ui_passworddialog.h" line="135"/>
+        <source>Password:</source>
+        <translation>Kennwort:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="43"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
+Please enter your password.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; benötigt root-Rechte.
+Bitte Kennwort eingeben.</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="54"/>
+        <source>Attempt #%1</source>
+        <translation>Versuch #%1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="57"/>
+        <source>Usage: %1 command [arguments...]
+
+GUI frontend for %2
+
+Arguments:
+  command        Command to run.
+  arguments      Optional arguments for command.
+
+</source>
+        <translation>Verwendung: %1 command [arguments...]
+
+graphische Benutzeroberfläche für %2
+
+Argumente:
+  command        auszuführender Befehl
+  arguments      Argumente zum auszuführenden Befehl
+
+</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>%1 version %2
+</source>
+        <translation>%1 Version %2
+</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="80"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1: kein auszuführender Befehl angegeben!</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="131"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation>nachgeordneter Prozess &apos;%1&apos; ist fehlgeschlagen!
+%2</translation>
+    </message>
+</context>
+</TS>
