a2a4536e8b332b82b8d0f7445dbe047f485b01f2
Kin Lo <kinabcd@gmail.com>
2015-08-22 11:31:15 +0800

    Traditional Chinese translation

---
+++ translations/lxqt-sudo_zh_TW.ts
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../passworddialog.ui" line="20"/>
+        <location filename="../build/ui_passworddialog.h" line="129"/>
+        <source>LXQt sudo</source>
+        <translation>LXQt sudo</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="77"/>
+        <location filename="../build/ui_passworddialog.h" line="134"/>
+        <source>Command:</source>
+        <translation>指令：</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="84"/>
+        <location filename="../build/ui_passworddialog.h" line="135"/>
+        <source>Password:</source>
+        <translation>密碼：</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="43"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
+Please enter your password.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; 需要管理員權限
+請輸入您的密碼 </translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="54"/>
+        <source>Attempt #%1</source>
+        <translation>嘗試 %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="57"/>
+        <source>Usage: %1 command [arguments...]
+
+GUI frontend for %2
+
+Arguments:
+  command        Command to run.
+  arguments      Optional arguments for command.
+
+</source>
+<translation>用法：%1 指令 [選項...]
+
+%2 的圖型前端
+
+選項：
+  指令           要執行的指令
+  選項           給指令的選項
+
+</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>%1 version %2
+</source>
+        <translation>%1 版本 %s</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="80"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1：沒有可執行的指令</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="131"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation>子執行序&apos;%1&apos;執行失敗！
+%2</translation>
+    </message>
+</context>
+</TS>
