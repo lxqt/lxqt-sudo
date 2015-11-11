af22cf7cb66ec85c618779d34ee9c3c80ad4cd9b
Vincenzo Reale <smart2128@baslug.org>
2015-11-11 09:00:04 +0100

    Italian translation update

--- translations/lxqt-sudo_it.ts
+++ translations/lxqt-sudo_it.ts
@@ -1,18 +1,18 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="it">
+<TS version="2.0" language="it">
 <context>
     <name>Communication</name>
     <message>
         <location filename="../communication.cpp" line="85"/>
         <source>%1: Communication - unable to create shared memory(%2B), </source>
-        <translation>%1: Comunicazione - non è stato possibile creare memoria condivisa(%2B), </translation>
+        <translation>%1: Comunicazione - impossibile creare memoria condivisa(%2B), </translation>
     </message>
     <message>
         <location filename="../communication.cpp" line="183"/>
         <source>%1: Communication - password is too long(%2) for aquired shared memory(%3), has to be cut-off...
 </source>
-        <translation>%1: Comunicazione - password troppo lunga(%2) per acquisire memoria condivisa(%3) deve essere accorciata...
+        <translation>%1: Comunicazione - la password è troppo lunga(%2) per acquisire memoria condivisa(%3) deve essere accorciata...
 </translation>
     </message>
 </context>
@@ -22,7 +22,7 @@
         <location filename="../passworddialog.ui" line="20"/>
         <location filename="../build/ui_passworddialog.h" line="129"/>
         <source>LXQt sudo</source>
-        <translation></translation>
+        <translation>sudo di LXQt</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="77"/>
@@ -34,14 +34,14 @@
         <location filename="../passworddialog.ui" line="84"/>
         <location filename="../build/ui_passworddialog.h" line="135"/>
         <source>Password:</source>
-        <translation></translation>
+        <translation>Password:</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="43"/>
         <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
 Please enter your password.</source>
         <translation>&lt;b&gt;%1&lt;/b&gt; richiede privilegi di amministratore.
-Per favore inserire la password.</translation>
+Digita la tua password.</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="53"/>
@@ -64,7 +64,7 @@ Arguments:
 </source>
         <translation>Uso: %1 comando [argomenti...]
 
-Frontend grafico per %2
+Interfaccia grafica per %2
 
 Argomenti:
   comando        Comando da eseguire.
@@ -81,13 +81,13 @@ Argomenti:
     <message>
         <location filename="../main.cpp" line="81"/>
         <source>%1: no command to run provided!</source>
-        <translation>%1: non è stato immesso nessun comando da eseguire!</translation>
+        <translation>%1: non è stato fornito alcun comando da eseguire!</translation>
     </message>
     <message>
         <location filename="../main.cpp" line="156"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
-        <translation>Sottoprocesso %1 fallito!
+        <translation>Sottoprocesso %1 non riuscito!
 %2</translation>
     </message>
 </context>
