93756f5761304f4c7d4e551703818a62e88ae08c
Robert Antoni Buj Gelonch <rbuj@fedoraproject.org>
2016-08-11 00:04:11 +0200

    Add lxqt-sudo_ca.ts (Catalan)

---
+++ lxqt-sudo_ca.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../../passworddialog.ui" line="20"/>
+        <source>LXQt sudo</source>
+        <translation>sudo de LXQt</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.ui" line="77"/>
+        <source>Command:</source>
+        <translation>Ordre:</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.ui" line="84"/>
+        <source>Password:</source>
+        <translation>Contrasenya:</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.cpp" line="43"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
+Please enter your password.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; necessita privilegis administratius.
+Introduïu la vostra contrasenya.</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.cpp" line="54"/>
+        <source>Attempt #%1</source>
+        <translation>Intent núm. %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../sudo.cpp" line="63"/>
+        <source>Usage: %1 option [command [arguments...]]
+
+GUI frontend for %2/%3
+
+Arguments:
+  option:
+    -h|--help      Print this help.
+    -v|--version   Print version information.
+    -s|--su        Use %3(1) as backend.
+    -d|--sudo      Use %2(8) as backend.
+  command          Command to run.
+  arguments        Optional arguments for command.
+
+</source>
+        <translation>Ús: %1 opció [ordre [arguments...]]
+
+Interfície gràfica per a %2/%3
+
+Arguments:
+  opció:
+    -h|--help      Imprimeix aquesta ajuda.
+    -v|--version   Imprimeix la informació de la versió.
+    -s|--su        Utilitza %3(1) com a rerefons.
+    -d|--sudo      Utilitza %2(8) com a rerefons.
+  ordre            L&apos;ordre a executar.
+  arguments        Els arguments opcionals per a l&apos;ordre.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="80"/>
+        <source>%1 version %2
+</source>
+        <translation>%1 versió %2
+</translation>
+    </message>
+</context>
+<context>
+    <name>Sudo</name>
+    <message>
+        <location filename="../../sudo.cpp" line="135"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1: no s&apos;ha proporcionat cap ordre a executar!</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="142"/>
+        <source>%1: no backend chosen!</source>
+        <translation>%1: no s&apos;ha triat cap rerefons!</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="147"/>
+        <source>%1: warning - got multiple arguments for %2 backend, squashing into one: %3</source>
+        <translation>%1: advertència - tenía múltiples arguments per al rerefons %2, s&apos;aixafen a un: %3</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="162"/>
+        <source>Failed to fork: %1</source>
+        <translation>El fork ha fallat: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="205"/>
+        <source>%1: Failed to exec &apos;%2&apos;: %3
+</source>
+        <translation>%1: L&apos;exec ha fallat &apos;%2&apos;: %3
+</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="234"/>
+        <source>Failed to set non-block: %1</source>
+        <translation>El set non-block ha fallat: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="242"/>
+        <source>Failed to fdopen: %1</source>
+        <translation>El fdopen ha fallat: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="275"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation>El procés fill &apos;%1&apos; ha fallat!
+%2</translation>
+    </message>
+</context>
+</TS>
