dbfae14734f4c0b7f6fe69959033cde19be73480
scootergrisen <scootergrisen@gmail.com>
2016-12-14 02:46:15 +0100

    Update Danish translations

---
+++ lxqt-sudo_da.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../../passworddialog.ui" line="20"/>
+        <source>LXQt sudo</source>
+        <translation>LXQt sudo</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.ui" line="77"/>
+        <source>Command:</source>
+        <translation>Kommando:</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.ui" line="84"/>
+        <source>Password:</source>
+        <translation>Adgangskode:</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.cpp" line="43"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
+Please enter your password.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; behøver administrative rettigheder.
+Indtast venligst din adgangskode.</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.cpp" line="54"/>
+        <source>Attempt #%1</source>
+        <translation>Forsøg nummer %1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../sudo.cpp" line="63"/>
+        <source>Usage: %1 option [command [arguments...]]
+
+GUI frontend for %2/%3
+
+Arguments:
+  option:
+    -h|--help      Print this help.
+    -v|--version   Print version information.
+    -s|--su        Use %3(1) as backend.
+    -d|--sudo      Use %2(8) as backend.
+  command          Command to run.
+  arguments        Optional arguments for command.
+
+</source>
+        <translation>Anvendelse: %1 valgmulighed [kommando [argumenter...]]
+
+Grafisk brugerflade til %2/%3
+
+Argumenter:
+  valgmulighed:
+    -h|--help      Vis denne hjælp.
+    -v|--version   Vis information om version.
+    -s|--su        Brug %3(1) som backend.
+    -d|--sudo      Brug %2(8) som backend.
+  kommando          Kommando som skal køres.
+  argumenter        Valgfrie argumenter til kommando.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="80"/>
+        <source>%1 version %2
+</source>
+        <translation>%1 version %2
+</translation>
+    </message>
+</context>
+<context>
+    <name>Sudo</name>
+    <message>
+        <location filename="../../sudo.cpp" line="135"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1: ingen angivet kommando som skal køres!</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="142"/>
+        <source>%1: no backend chosen!</source>
+        <translation>%1: ingen backend valgt!</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="147"/>
+        <source>%1: warning - got multiple arguments for %2 backend, squashing into one: %3</source>
+        <translation>%1: advarsel - gik adskillige argumenter til %2-backend, samler dem i ét: %3</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="162"/>
+        <source>Failed to fork: %1</source>
+        <translation>Kunne ikke fork: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="205"/>
+        <source>%1: Failed to exec &apos;%2&apos;: %3
+</source>
+        <translation>%1: kunne ikke exec &apos;%2&apos;: %3
+</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="234"/>
+        <source>Failed to set non-block: %1</source>
+        <translation>Kunne ikke sætte ikke-blok: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="242"/>
+        <source>Failed to fdopen: %1</source>
+        <translation>Kunne ikke fdopen: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="275"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation>Barneprocessen &apos;%1&apos; fejlede!
+%2</translation>
+    </message>
+</context>
+</TS>
