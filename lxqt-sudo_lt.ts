88fea2e176b2f59c6587331a5187c2127272a43e
welaq <hazap@hotmail.com>
2017-05-23 02:18:44 +0300

    Lithuanian translatio

---
+++ lxqt-sudo_lt.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lt">
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../../passworddialog.ui" line="20"/>
+        <source>LXQt sudo</source>
+        <translation>LXQt sudo</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.ui" line="77"/>
+        <source>Command:</source>
+        <translation>Komanda:</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.ui" line="84"/>
+        <source>Password:</source>
+        <translation>Slaptažodis:</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.cpp" line="43"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
+Please enter your password.</source>
+        <translation>&lt;b&gt;%1&lt;/b&gt; reikalauja administratoriaus teisių.
+Prašome įrašyti savo slaptažodį.</translation>
+    </message>
+    <message>
+        <location filename="../../passworddialog.cpp" line="54"/>
+        <source>Attempt #%1</source>
+        <translation>Bandymas #%1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../sudo.cpp" line="63"/>
+        <source>Usage: %1 option [command [arguments...]]
+
+GUI frontend for %2/%3
+
+Arguments:
+  option:
+    -h|--help      Print this help.
+    -v|--version   Print version information.
+    -s|--su        Use %3(1) as backend.
+    -d|--sudo      Use %2(8) as backend.
+  command          Command to run.
+  arguments        Optional arguments for command.
+
+</source>
+        <translation>Naudojimas: %1 parametras [komanda [argumentai...]]
+
+Grafinė naudotojo sąsaja, skirta %2/%3
+
+Argumentai:
+  parametras:
+    -h|--help      Išvesti šią pagalbą.
+    -v|--version   Išvesti versijos informacija.
+    -s|--su        Naudoti %3(1) kaip vidinę pusę.
+    -d|--sudo      Naudoti %2(8) kaip vidinę pusę.
+  command          Komanda, kurią vykdyti.
+  arguments        Pasirinktini argumentai komandai.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="80"/>
+        <source>%1 version %2
+</source>
+        <translation>%1 versija %2
+</translation>
+    </message>
+</context>
+<context>
+    <name>Sudo</name>
+    <message>
+        <location filename="../../sudo.cpp" line="135"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1: nepateikta komanda, kurią vykdyti!</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="142"/>
+        <source>%1: no backend chosen!</source>
+        <translation>%1: nepasirinkta vidinė pusė!</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="147"/>
+        <source>%1: warning - got multiple arguments for %2 backend, squashing into one: %3</source>
+        <translation>%1: įspėjimas - gauti keli argumentai, skirti %2 vidinei pusei, juos sujungiama į vieną: %3</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="162"/>
+        <source>Failed to fork: %1</source>
+        <translation>Nepavyko iššakoti: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="205"/>
+        <source>%1: Failed to exec &apos;%2&apos;: %3
+</source>
+        <translation>%1: Nepavyko įvykdyti &quot;%2&quot;: %3
+</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="234"/>
+        <source>Failed to set non-block: %1</source>
+        <translation>Nepavyko nustatyti neblokuojančiu: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="242"/>
+        <source>Failed to fdopen: %1</source>
+        <translation>Nepavyko atlikti fdopen: %1</translation>
+    </message>
+    <message>
+        <location filename="../../sudo.cpp" line="275"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation>Vyksnis &quot;%1&quot; nepavyko!
+%2</translation>
+    </message>
+</context>
+</TS>
