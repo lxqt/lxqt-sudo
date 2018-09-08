0c8820c98af461acbc58527ae20dfe389cc489d6
scootergrisen <scootergrisen@gmail.com>
2018-09-08 06:31:45 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/da/

--- translations/lxqt-sudo_da.ts
+++ translations/lxqt-sudo_da.ts
@@ -27,7 +27,7 @@ Indtast venligst din adgangskode.</translation>
     <message>
         <location filename="../passworddialog.cpp" line="41"/>
         <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; behøver administrative rettigheder.&lt;br&gt;Indtast venligst din adgangskode.</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="52"/>
@@ -101,7 +101,8 @@ Argumenter:
         <location filename="../sudo.cpp" line="240"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exitting!
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1: Registrerede forsøg på at indføre privilegeret kommando via LC_ALL env(%2). Afslutter!
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="259"/>
