7de7cf519f4365ca8e95145d2adffbee6074dcf3
Fedor Piecka <fpiecka@neviem.eu>
2018-09-27 11:11:36 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100,0% (17 of 17 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/sk/

--- translations/lxqt-sudo_sk.ts
+++ translations/lxqt-sudo_sk.ts
@@ -27,7 +27,7 @@ Prosím, zadajte svoje heslo.</translation>
     <message>
         <location filename="../passworddialog.cpp" line="41"/>
         <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; si vyžaduje oprávnenia správcu.&lt;br&gt;Zadajte, prosím, svoje heslo.</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="52"/>
@@ -101,7 +101,8 @@ Parametre:
         <location filename="../sudo.cpp" line="240"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exitting!
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1: Bol detegovaný pokus o vloženie privilegovaného príkazu cez LC_ALL env(%2). Ukončujem program!
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="259"/>
