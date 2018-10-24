cb0a98b95a7662d0bfd4d542efecd21a9c2e432a
Einar Mostad <einar@mostad.eu>
2018-10-24 18:56:38 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ translations/lxqt-sudo_nb_NO.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nb_NO">
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../passworddialog.ui" line="14"/>
+        <source>LXQt sudo</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="77"/>
+        <source>Command:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="84"/>
+        <source>Password:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="41"/>
+        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.&lt;br&gt;Please enter your password.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="52"/>
+        <source>Attempt #%1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../sudo.cpp" line="67"/>
+        <source>Usage: %1 option [command [arguments...]]
+
+GUI frontend for %2/%3
+
+Arguments:
+  option:
+    -h|--help      Print this help.
+    -v|--version   Print version information.
+    -s|--su        Use %3(1) as backend.
+    -d|--sudo      Use %2(8) as backend.
+  command          Command to run.
+  arguments        Optional arguments for command.
+
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="84"/>
+        <source>%1 version %2
+</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Sudo</name>
+    <message>
+        <location filename="../sudo.cpp" line="170"/>
+        <source>%1: no command to run provided!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="177"/>
+        <source>%1: no backend chosen!</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="197"/>
+        <source>Failed to fork: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="240"/>
+        <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exitting!
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="259"/>
+        <source>%1: Failed to exec &apos;%2&apos;: %3
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="282"/>
+        <source>Failed to set non-block: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="290"/>
+        <source>Failed to fdopen: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="321"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
