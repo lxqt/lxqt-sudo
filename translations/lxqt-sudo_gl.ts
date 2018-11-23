97c48e3757505b704daebd35016b798af915fb0a
Miguel A. Bouzada <mbouzada@gmail.com>
2018-11-23 18:43:23 +0000

    Translated using Weblate (Galician)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/gl/

--- translations/lxqt-sudo_gl.ts
+++ translations/lxqt-sudo_gl.ts
@@ -11,22 +11,22 @@
     <message>
         <location filename="../passworddialog.ui" line="77"/>
         <source>Command:</source>
-        <translation></translation>
+        <translation>Orde:</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="84"/>
         <source>Password:</source>
-        <translation></translation>
+        <translation>Contrasinal:</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="41"/>
         <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; necesita privilexios administrativos.&lt;br&gt;Introduza o seu contrasinal.</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="52"/>
         <source>Attempt #%1</source>
-        <translation></translation>
+        <translation>Intento número %1</translation>
     </message>
 </context>
 <context>
@@ -53,7 +53,8 @@ Arguments:
         <location filename="../sudo.cpp" line="84"/>
         <source>%1 version %2
 </source>
-        <translation></translation>
+        <translation>%1 versión %2
+</translation>
     </message>
 </context>
 <context>
@@ -61,45 +62,48 @@ Arguments:
     <message>
         <location filename="../sudo.cpp" line="170"/>
         <source>%1: no command to run provided!</source>
-        <translation></translation>
+        <translation>%1: non forneceu ningunha orde!</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="177"/>
         <source>%1: no backend chosen!</source>
-        <translation></translation>
+        <translation>%1: non se escolleu ningunha infraestrutura!</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="197"/>
         <source>Failed to fork: %1</source>
-        <translation></translation>
+        <translation>Produciuse un fallo ao bifurcar: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="240"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exitting!
 </source>
-        <translation></translation>
+        <translation>%1: Detectouse un intento de inxectar unha orde con privilexios a través de LC_ALL env(%2). Saíndo!
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="259"/>
         <source>%1: Failed to exec &apos;%2&apos;: %3
 </source>
-        <translation></translation>
+        <translation>%1: Produciuse un fallo ao executar «%2»: %3
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="282"/>
         <source>Failed to set non-block: %1</source>
-        <translation></translation>
+        <translation>Produciuse un fallo ao estabelecer o non bloqueo: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="290"/>
         <source>Failed to fdopen: %1</source>
-        <translation></translation>
+        <translation>Produciuse un fallo en fdopen: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="321"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
-        <translation></translation>
+        <translation>Produciuse un fallo no proceso fillo «%1»!
+%2</translation>
     </message>
 </context>
 </TS>
