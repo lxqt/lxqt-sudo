ffa19328b1ed08e8518681e49770d31c11d34b8b
Einar Mostad <einar@mostad.eu>
2018-11-25 00:29:56 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/nb_NO/

--- translations/lxqt-sudo_nb_NO.ts
+++ translations/lxqt-sudo_nb_NO.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../passworddialog.ui" line="14"/>
         <source>LXQt sudo</source>
-        <translation></translation>
+        <translation>LXQt kjør som administrator</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="77"/>
         <source>Command:</source>
-        <translation></translation>
+        <translation>Kommando:</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="84"/>
@@ -21,12 +21,12 @@
     <message>
         <location filename="../passworddialog.cpp" line="41"/>
         <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation></translation>
+        <translation>&lt;b&gt;%1&lt;/b&gt; trenger administrative privilegier.&lt;br&gt;Skriv inn passordet ditt.</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="52"/>
         <source>Attempt #%1</source>
-        <translation></translation>
+        <translation>Forsøk #%1</translation>
     </message>
 </context>
 <context>
@@ -47,13 +47,27 @@ Arguments:
   arguments        Optional arguments for command.
 
 </source>
-        <translation></translation>
+        <translation>Bruk: %1 valg [kommandd [argumenter...]]
+
+GUI-visning for %2/%3
+
+Argumenter:
+  valg:
+    -h|--help      Skriv denne hjelpen.
+    -v|--version  Skriv versjonsinformasjon.
+    -s|--su         Bruk %3(1) som backend.
+    -d|--sudo     Bruk %2(8) som backend.
+  kommando          Kommand å kjøre.
+  argumenter         Valgfrie argumenter for kommandoen.
+
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="84"/>
         <source>%1 version %2
 </source>
-        <translation></translation>
+        <translation>%1 versjon %2
+</translation>
     </message>
 </context>
 <context>
@@ -61,45 +75,48 @@ Arguments:
     <message>
         <location filename="../sudo.cpp" line="170"/>
         <source>%1: no command to run provided!</source>
-        <translation></translation>
+        <translation>%1: ingen kommando å kjøre!</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="177"/>
         <source>%1: no backend chosen!</source>
-        <translation></translation>
+        <translation>%1: ingen backend valgt!</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="197"/>
         <source>Failed to fork: %1</source>
-        <translation></translation>
+        <translation>Feilet ved deling: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="240"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exitting!
 </source>
-        <translation></translation>
+        <translation>%1: Oppdaget forsøk på å skyte inn kommando med økte privilegier via LC_ALL env(%2). Avslutter!
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="259"/>
         <source>%1: Failed to exec &apos;%2&apos;: %3
 </source>
-        <translation></translation>
+        <translation>%1: Kunne ikke kjøre &apos;%2&apos;: %3
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="282"/>
         <source>Failed to set non-block: %1</source>
-        <translation></translation>
+        <translation>Kunne ikke blokkere: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="290"/>
         <source>Failed to fdopen: %1</source>
-        <translation></translation>
+        <translation>Kunne ikke fdopen: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="321"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
-        <translation></translation>
+        <translation>Underprossessn &apos;%1&apos; feilet!
+%2</translation>
     </message>
 </context>
 </TS>
