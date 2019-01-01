ce34c2f70f33e1f5852b7344b6963d0d31549ab3
Moo <hazap@hotmail.com>
2019-01-01 14:08:28 +0000

    Translated using Weblate (Lithuanian)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/lt/

--- translations/lxqt-sudo_lt.ts
+++ translations/lxqt-sudo_lt.ts
@@ -85,23 +85,24 @@ Argumentai:
     <message>
         <location filename="../sudo.cpp" line="197"/>
         <source>Syscall error, failed to fork: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Syscall klaida, nepavyko atšakoti: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="240"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1: Aptiktas bandymas įskiepyti privilegijuotą komandą per LC_ALL env(%2). Išeinama!
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="282"/>
         <source>Syscall error, failed to bring pty to non-block mode: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Syscall klaida, nepavyko pristatyti pty į neblokavimo veikseną: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="290"/>
         <source>Syscall error, failed to fdopen pty: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Syscall klaida, nepavyko atlikti fdopen pty: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="259"/>
