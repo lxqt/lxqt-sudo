2a65d347bed54bbea3bf81cfe28e0f73e534a4ba
Alf Gaida <agaida@siduction.org>
2019-01-20 17:28:10 +0100

    Translation update

--- translations/lxqt-sudo_es.ts
+++ translations/lxqt-sudo_es.ts
@@ -4,27 +4,52 @@
 <context>
     <name>PasswordDialog</name>
     <message>
-        <location filename="../passworddialog.ui" line="14"/>
+        <location filename="../passworddialog.ui" line="6"/>
         <source>LXQt sudo</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../passworddialog.ui" line="77"/>
+        <location filename="../passworddialog.ui" line="42"/>
+        <source>Copy command to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="45"/>
+        <source>&amp;Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="83"/>
+        <source>The requested action needs administrative privileges.&lt;br&gt;Please enter your password.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="106"/>
+        <source>LXQt sudo backend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="109"/>
+        <source>A program LXQt sudo calls in background to elevate priveledges.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="119"/>
         <source>Command:</source>
         <translation>Comando:</translation>
     </message>
     <message>
-        <location filename="../passworddialog.ui" line="84"/>
+        <location filename="../passworddialog.ui" line="126"/>
         <source>Password:</source>
         <translation>Contraseña:</translation>
     </message>
     <message>
-        <location filename="../passworddialog.cpp" line="41"/>
-        <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation>&lt;b&gt;%1&lt;/b&gt; necesita privilegios administrativos.&lt;br&gt;Introduzca su contraseña.</translation>
+        <location filename="../passworddialog.ui" line="133"/>
+        <source>Enter password</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../passworddialog.cpp" line="52"/>
+        <location filename="../passworddialog.cpp" line="60"/>
         <source>Attempt #%1</source>
         <translation>Intento número %1</translation>
     </message>
@@ -73,46 +98,52 @@ Argumentos:
 <context>
     <name>Sudo</name>
     <message>
-        <location filename="../sudo.cpp" line="170"/>
+        <location filename="../sudo.cpp" line="183"/>
         <source>%1: no command to run provided!</source>
         <translation>%1: ¡no se ha dado ningún comando!</translation>
     </message>
     <message>
-        <location filename="../sudo.cpp" line="177"/>
+        <location filename="../sudo.cpp" line="190"/>
         <source>%1: no backend chosen!</source>
         <translation>%1: ¡no se ha elegido ninguna aplicación de fondo!</translation>
     </message>
     <message>
-        <location filename="../sudo.cpp" line="197"/>
+        <location filename="../sudo.cpp" line="207"/>
         <source>Syscall error, failed to fork: %1</source>
         <translation>Error de llamada al sistema, no se ha podido crear el subproceso: %1</translation>
     </message>
     <message>
-        <location filename="../sudo.cpp" line="240"/>
+        <location filename="../sudo.cpp" line="234"/>
+        <source>unset</source>
+        <extracomment>shouldn&apos;t be actually used but keep as short as possible in translations just in case.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="270"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
 </source>
         <translation>%1: Se ha detectado un intento de inyectar un comando con privilegios mediante LC_ALL env(%2). ¡Cancelado!
 </translation>
     </message>
     <message>
-        <location filename="../sudo.cpp" line="282"/>
+        <location filename="../sudo.cpp" line="314"/>
         <source>Syscall error, failed to bring pty to non-block mode: %1</source>
         <translation>Error de llamada al sistema, no se ha podido poner pty en modo desbloqueado: %1</translation>
     </message>
     <message>
-        <location filename="../sudo.cpp" line="290"/>
+        <location filename="../sudo.cpp" line="322"/>
         <source>Syscall error, failed to fdopen pty: %1</source>
         <translation>Error de llamada al sistema, no se ha podido hacer fdopen pty: %1</translation>
     </message>
     <message>
-        <location filename="../sudo.cpp" line="259"/>
+        <location filename="../sudo.cpp" line="291"/>
         <source>%1: Failed to exec &apos;%2&apos;: %3
 </source>
         <translation>%1: Ha fallado al ejecutar &apos;%2&apos;: %3
 </translation>
     </message>
     <message>
-        <location filename="../sudo.cpp" line="321"/>
+        <location filename="../sudo.cpp" line="353"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
         <translation>¡El proceso hijo &apos;%1&apos; ha fallado!
