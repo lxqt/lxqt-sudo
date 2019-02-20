9c53dd505a48bdd37beac89507bbf35e7d9bf1ec
Sérgio Marques <smarquespt@gmail.com>
2019-02-20 13:44:28 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/pt/

--- translations/lxqt-sudo_pt.ts
+++ translations/lxqt-sudo_pt.ts
@@ -21,17 +21,17 @@
     <message>
         <location filename="../passworddialog.ui" line="83"/>
         <source>The requested action needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation>A ação requerida necessita de privilégios administrativos.&lt;br&gt;Por favor insira sua senha.</translation>
+        <translation>A ação solicitada necessita de privilégios administrativos.&lt;br&gt;Introduza a sua palavra-passe.</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="106"/>
         <source>LXQt sudo backend</source>
-        <translation>LXQt sudo backend</translation>
+        <translation>Infraestrutura LXQt sudo</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="109"/>
         <source>A program LXQt sudo calls in background to elevate priveledges.</source>
-        <translation>Um programa LXQt sudo a correr em segundo plano pede para elevar privilégios</translation>
+        <translation>Um programa LXQt sudo a correr em segundo plano pede para elevar privilégios.</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="119"/>
@@ -46,7 +46,7 @@
     <message>
         <location filename="../passworddialog.ui" line="133"/>
         <source>Enter password</source>
-        <translation type="unfinished"></translation>
+        <translation>Introduza a palavra-passe</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="60"/>
@@ -110,29 +110,30 @@ Argumentos:
     <message>
         <location filename="../sudo.cpp" line="207"/>
         <source>Syscall error, failed to fork: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro &apos;syscall&apos;, falha ao clonar: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="234"/>
         <source>unset</source>
         <extracomment>shouldn&apos;t be actually used but keep as short as possible in translations just in case.</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>indefinido</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="270"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1: Detetada uma tentativa de injetar um comando privilegiado via LC_ALL (%2). A sair!
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="314"/>
         <source>Syscall error, failed to bring pty to non-block mode: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro &apos;syscall&apos;, falha ao trazer pty para modo não bloco: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="322"/>
         <source>Syscall error, failed to fdopen pty: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro &apos;syscall&apos;, falha ao fdopen pty: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="291"/>
@@ -145,7 +146,7 @@ Argumentos:
         <location filename="../sudo.cpp" line="353"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
-        <translation>Falha no processodependente %1!
+        <translation>Falha no processo dependente %1!
 %2</translation>
     </message>
 </context>
