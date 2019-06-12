fd6d07a015202341a8d1b9e4d3f9fd1badb4641e
BavonChao <chaochinghua@outlook.com>
2019-06-12 08:54:30 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 90.5% (19 of 21 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/zh_TW/

--- translations/lxqt-sudo_zh_TW.ts
+++ translations/lxqt-sudo_zh_TW.ts
@@ -31,7 +31,7 @@
     <message>
         <location filename="../passworddialog.ui" line="109"/>
         <source>A program LXQt sudo calls in background to elevate priveledges.</source>
-        <translation type="unfinished"></translation>
+        <translation>在背景中由LXQT sudo 調用以提升權限的程式。</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="119"/>
@@ -110,7 +110,7 @@ GUI 前端 %2/%3
     <message>
         <location filename="../sudo.cpp" line="207"/>
         <source>Syscall error, failed to fork: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>系統調用錯誤，復刻%1失敗</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="234"/>
@@ -122,17 +122,18 @@ GUI 前端 %2/%3
         <location filename="../sudo.cpp" line="270"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1：檢測到嘗試通過LC_ALL env（％2）注入特權命令。退出！
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="314"/>
         <source>Syscall error, failed to bring pty to non-block mode: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">系統調用錯誤，無法將pty調整為非阻止模式：%1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="322"/>
         <source>Syscall error, failed to fdopen pty: %1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">系統調用錯誤，開啓pty失敗：%1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="291"/>
