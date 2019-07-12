d3e0b152808b7aa85158e6eb3055bd5e9bb0c007
Enol P <enolp@softastur.org>
2019-07-12 13:24:25 +0000

    Translated using Weblate (Asturian)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/ast/

--- translations/lxqt-sudo_ast.ts
+++ translations/lxqt-sudo_ast.ts
@@ -6,52 +6,52 @@
     <message>
         <location filename="../passworddialog.ui" line="6"/>
         <source>LXQt sudo</source>
-        <translation type="unfinished"></translation>
+        <translation>LXQt sudo</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="42"/>
         <source>Copy command to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Copiar el comandu al cartafueyu</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="45"/>
         <source>&amp;Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Copiar</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="83"/>
         <source>The requested action needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation type="unfinished"></translation>
+        <translation>L&apos;aición solicitada precisa privilexos alministrativos.&lt;br&gt;Introduz la contraseña, por favor.</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="106"/>
         <source>LXQt sudo backend</source>
-        <translation type="unfinished"></translation>
+        <translation>Backend de LXQt sudo</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="109"/>
         <source>A program LXQt sudo calls in background to elevate priveledges.</source>
-        <translation type="unfinished"></translation>
+        <translation>Un programa que LXQt sudo llama en segundu planu pa ganar privilexos.</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="119"/>
         <source>Command:</source>
-        <translation type="unfinished"></translation>
+        <translation>Comandu:</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="126"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Contraseña:</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="133"/>
         <source>Enter password</source>
-        <translation type="unfinished"></translation>
+        <translation>Introduz</translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="60"/>
         <source>Attempt #%1</source>
-        <translation type="unfinished"></translation>
+        <translation>Intentu #%1</translation>
     </message>
 </context>
 <context>
@@ -72,13 +72,27 @@ Arguments:
   arguments        Optional arguments for command.
 
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Usu: %1 opción  [comandu [argumentos...]]
+
+Frontal GUI pa %2/%3
+
+Argumentos:
+  optción:
+    -h|--help      Amuesa esta ayuda.
+    -v|--version   Amuesa la información de la versión.
+    -s|--su        Usa %3(1) como backend.
+    -d|--sudo      Usa %2(8) como backend.
+  comandu          Comandu a executar.
+  argumentos        Argumentos opcionales del comandu.
+
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="84"/>
         <source>%1 version %2
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1 versión %2
+</translation>
     </message>
 </context>
 <context>
@@ -86,51 +100,54 @@ Arguments:
     <message>
         <location filename="../sudo.cpp" line="183"/>
         <source>%1: no command to run provided!</source>
-        <translation type="unfinished"></translation>
+        <translation>%1: ¡nun s&apos;apurrió dengún comandu a executar!</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="190"/>
         <source>%1: no backend chosen!</source>
-        <translation type="unfinished"></translation>
+        <translation>%1: ¡nun s&apos;escoyó dengún backend!</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="207"/>
         <source>Syscall error, failed to fork: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Fallu de llamada al sistema, fallu al biforcar: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="234"/>
         <source>unset</source>
         <extracomment>shouldn&apos;t be actually used but keep as short as possible in translations just in case.</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>nun s&apos;afitó</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="270"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1: Deteutóse un intentu d&apos;inyectar un comandu con privilexos pela variable d&apos;entornu(%2) LC_ALL. ¡Colando!
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="314"/>
         <source>Syscall error, failed to bring pty to non-block mode: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Fallu de llamada al sistema, fallu al poner la pty nel mou non-block: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="322"/>
         <source>Syscall error, failed to fdopen pty: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Fallu de llamada al sistema, fallu al facer fdopen a la pty: %1</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="291"/>
         <source>%1: Failed to exec &apos;%2&apos;: %3
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1: Fallu al facer exec a «%2»: %3
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="353"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
-        <translation type="unfinished"></translation>
+        <translation>¡El procesu fíu «%1» falló!
+%2</translation>
     </message>
 </context>
 </TS>
