81478688a551b7748d62f2060302e34f453fdefa
liu lizhi <kz-xy@163.com>
2019-09-11 12:40:35 +0000

    Translated using Weblate (Chinese (Simplified))
    Currently translated at 90.5% (19 of 21 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/zh_CN/

--- translations/lxqt-sudo_zh_CN.ts
+++ translations/lxqt-sudo_zh_CN.ts
@@ -122,7 +122,8 @@ Arguments:
         <location filename="../sudo.cpp" line="270"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
 </source>
-        <translation type="unfinished"></translation>
+        <translation>%1: 检测到尝试通过 LC_ALL env(%2) 注入权限命令。正在退出！
+</translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="314"/>
