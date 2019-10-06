a51e702ce1cdd5bf3fae11a06a3ca86e4fdc1ba9
Sérgio Marques <smarquespt@gmail.com>
2019-10-06 23:00:32 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/pt/

--- translations/lxqt-sudo_pt.ts
+++ translations/lxqt-sudo_pt.ts
@@ -37,7 +37,7 @@
         <location filename="../passworddialog.ui" line="109"/>
         <location filename="../obj-x86_64-linux-gnu/lxqt-sudo_autogen/include/ui_passworddialog.h" line="181"/>
         <source>A program LXQt sudo calls in background to elevate privileges.</source>
-        <translation type="unfinished"></translation>
+        <translation>Um programa LXQt su para elevar os privilégios de administrador.</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="119"/>
