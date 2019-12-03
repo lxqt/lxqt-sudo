c63627d0a0ed458780e25ad538801ead1cc6e2fc
Masamichi Ito <ito32bit+github@gmail.com>
2019-12-03 01:11:04 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/ja/

--- translations/lxqt-sudo_ja.ts
+++ translations/lxqt-sudo_ja.ts
@@ -31,13 +31,13 @@
         <location filename="../passworddialog.ui" line="106"/>
         <location filename="../obj-x86_64-linux-gnu/lxqt-sudo_autogen/include/ui_passworddialog.h" line="178"/>
         <source>LXQt sudo backend</source>
-        <translation>LXQt sudo バックエンド</translation>
+        <translation>LXQt sudo のバックエンドです</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="109"/>
         <location filename="../obj-x86_64-linux-gnu/lxqt-sudo_autogen/include/ui_passworddialog.h" line="181"/>
         <source>A program LXQt sudo calls in background to elevate privileges.</source>
-        <translation>プログラム LXQt sudo は権限を上げるためにバックグラウンドで呼び出されます。</translation>
+        <translation>権限を上げるために、プログラム「LXQt sudo」がバックグラウンドで呼び出します。</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="119"/>
