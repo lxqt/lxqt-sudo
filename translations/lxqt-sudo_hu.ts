fac1f6948d3bbee922a3ce12acd4156c481d9a88
Ács Zoltán <acszoltan111@gmail.com>
2020-01-25 18:06:49 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 66.7% (14 of 21 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/hu/

--- translations/lxqt-sudo_hu.ts
+++ translations/lxqt-sudo_hu.ts
@@ -25,7 +25,7 @@
         <location filename="../passworddialog.ui" line="83"/>
         <location filename="../obj-x86_64-linux-gnu/lxqt-sudo_autogen/include/ui_passworddialog.h" line="176"/>
         <source>The requested action needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation>A kért művelethez rendszergazdai jogosultságokra van szükség. &lt;br&gt; Kérjük, írja be a jelszavát.</translation>
+        <translation>A kért művelethez rendszergazdai jogosultság szükséges. &lt;br&gt;Írja be a jelszavát.</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="106"/>
