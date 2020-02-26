6c5090ed708f955f2014a931223a9ff9b108b79d
Paolo Vigoroso <xpaolox@zoznam.sk>
2020-02-26 07:33:50 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: LXQt/LXQt Sudo
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/lxqt-sudo/sk/

--- translations/lxqt-sudo_sk.ts
+++ translations/lxqt-sudo_sk.ts
@@ -19,7 +19,7 @@
         <location filename="../passworddialog.ui" line="45"/>
         <location filename="../obj-x86_64-linux-gnu/lxqt-sudo_autogen/include/ui_passworddialog.h" line="175"/>
         <source>&amp;Copy</source>
-        <translation>Kopírovať</translation>
+        <translation>&amp;Kopírovať</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="83"/>
