bbd38c4a9cc56e34d12907d2f44f92fcb312e029
A S Alam <amanpreet.alam@gmail.com>
2023-12-24 21:23:00 +0100

    Weblate commits
    Co-authored-by: A S Alam <amanpreet.alam@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-sudo/pa/
    Translation: LXQt Configuration/lxqt-sudo

---
+++ translations/lxqt-sudo_pa.ts
@@ -0,0 +1,137 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pa">
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../passworddialog.ui" line="6"/>
+        <source>LXQt sudo</source>
+        <translation>LXQt sudo</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="42"/>
+        <source>Copy command to clipboard</source>
+        <translation>ਕਮਾਂਡ ਨੂੰ ਕਲਿੱਪਬੋਰਡ ਵਿੱਚ ਕਾਪੀ ਕਰੋ</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="45"/>
+        <source>&amp;Copy</source>
+        <translation>ਕਾਪੀ(&amp;C)</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="83"/>
+        <source>The requested action needs administrative privileges.&lt;br&gt;Please enter your password.</source>
+        <translation>ਮੰਗ ਕੀਤੀ ਕਾਰਵਾਈ ਲਈ ਪਰਸ਼ਾਸ਼ਕੀ ਇਜ਼ਾਜ਼ਤਾਂ ਚਾਹੀਦੀਆਂ ਹਨ।&lt;br&gt;ਆਪਣਾ ਪਾਸਵਰਡ ਦਿਓ।</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="106"/>
+        <source>LXQt sudo backend</source>
+        <translation>LXQt sudo ਬੈਕਐਂਡ</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="109"/>
+        <source>A program LXQt sudo calls in background to elevate privileges.</source>
+        <translation>ਪਰੋਗਰਾਮ LXQt sudo ਬੈਕਗਰਾਊਂਡ ਵਿੱਚ ਵਾਧਾ ਮਿਲੀਆਂ ਇਜਾਜ਼ਤਾਂ ਨੂੰ ਵਰਤਦਾ ਹੈ।</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="119"/>
+        <source>Command:</source>
+        <translation>ਕਮਾਂਡ:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="126"/>
+        <source>Password:</source>
+        <translation>ਪਾਸਵਰਡ:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="133"/>
+        <source>Enter password</source>
+        <translation>ਪਾਸਵਰਡ ਦਿਓ</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="60"/>
+        <source>Attempt #%1</source>
+        <translation>ਕੋਸ਼ਿਸ਼ #%1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../sudo.cpp" line="75"/>
+        <source>Usage: %1 option [command [arguments...]]
+
+GUI frontend for %2/%3
+
+Arguments:
+  option:
+    -h|--help      Print this help.
+    -v|--version   Print version information.
+    -s|--su        Use %3(1) as backend.
+    -d|--sudo      Use %2(8) as backend.
+  command          Command to run.
+  arguments        Optional arguments for command.
+
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="92"/>
+        <source>%1 version %2
+</source>
+        <translation>%1 ਵਰਜ਼ਨ %2
+</translation>
+    </message>
+</context>
+<context>
+    <name>Sudo</name>
+    <message>
+        <location filename="../sudo.cpp" line="195"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1: ਕੋਈ ਕਮਾਂਡ ਨਹੀਂ ਦਿੱਤੀ ਹੈ!</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="202"/>
+        <source>%1: no backend chosen!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="219"/>
+        <source>Syscall error, failed to fork: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="246"/>
+        <source>unset</source>
+        <extracomment>shouldn&apos;t be actually used but keep as short as possible in translations just in case.</extracomment>
+        <translation>ਅਣ-ਸੈਟ</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="295"/>
+        <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="337"/>
+        <source>Syscall error, failed to bring pty to non-block mode: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="345"/>
+        <source>Syscall error, failed to fdopen pty: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="314"/>
+        <source>%1: Failed to exec &apos;%2&apos;: %3
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="376"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
