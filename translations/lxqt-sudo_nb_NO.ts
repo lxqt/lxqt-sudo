d0ae682bf4995da22a0b5e8097ee6c671e4b468f
Weblate <noreply@weblate.org>
2024-08-05 15:24:22 +0200

    Weblate commits
    Co-authored-by: ovl-1 <ovl-002@mailinator.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-sudo/nb_NO/
    Translation: LXQt Configuration/lxqt-sudo

--- translations/lxqt-sudo_nb_NO.ts
+++ translations/lxqt-sudo_nb_NO.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../passworddialog.ui" line="42"/>
         <source>Copy command to clipboard</source>
-        <translation>Kopier kommando til utklippsboken</translation>
+        <translation>Kopier kommando til utklippstavlen</translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="45"/>
