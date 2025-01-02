9486461e52d6f07e9fa34a24a583cc3a6591ea10
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2025-01-02 15:16:51 +0100

    Weblate commits (#221)
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-sudo/en_GB/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-sudo/ka/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-sudo/kab/
    Translation: LXQt Configuration/lxqt-sudo
    
    Co-authored-by: Andi Chandler <andi@gowling.com>
    Co-authored-by: ButterflyOfFire <butterflyoffire@protonmail.com>
    Co-authored-by: NorwayFun <temuri.doghonadze@gmail.com>

---
+++ translations/lxqt-sudo_en_GB.ts
@@ -0,0 +1,153 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB">
+<context>
+    <name>PasswordDialog</name>
+    <message>
+        <location filename="../passworddialog.ui" line="6"/>
+        <source>LXQt sudo</source>
+        <translation>LXQt sudo</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="42"/>
+        <source>Copy command to clipboard</source>
+        <translation>Copy command to clipboard</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="45"/>
+        <source>&amp;Copy</source>
+        <translation>&amp;Copy</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="83"/>
+        <source>The requested action needs administrative privileges.&lt;br&gt;Please enter your password.</source>
+        <translation>The requested action needs administrative privileges.&lt;br&gt;Please enter your password.</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="106"/>
+        <source>LXQt sudo backend</source>
+        <translation>LXQt sudo backend</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="109"/>
+        <source>A program LXQt sudo calls in background to elevate privileges.</source>
+        <translation>A program LXQt sudo calls in background to elevate privileges.</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="119"/>
+        <source>Command:</source>
+        <translation>Command:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="126"/>
+        <source>Password:</source>
+        <translation>Password:</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.ui" line="133"/>
+        <source>Enter password</source>
+        <translation>Enter password</translation>
+    </message>
+    <message>
+        <location filename="../passworddialog.cpp" line="60"/>
+        <source>Attempt #%1</source>
+        <translation>Attempt #%1</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../sudo.cpp" line="75"/>
+        <source>Usage: %1 option [command [arguments...]]
+
+GUI frontend for %2/%3
+
+Arguments:
+  option:
+    -h|--help      Print this help.
+    -v|--version   Print version information.
+    -s|--su        Use %3(1) as backend.
+    -d|--sudo      Use %2(8) as backend.
+  command          Command to run.
+  arguments        Optional arguments for command.
+
+</source>
+        <translation>Usage: %1 option [command [arguments...]]
+
+GUI frontend for %2/%3
+
+Arguments:
+  option:
+    -h|--help      Print this help.
+    -v|--version   Print version information.
+    -s|--su        Use %3(1) as backend.
+    -d|--sudo      Use %2(8) as backend.
+  command          Command to run.
+  arguments        Optional arguments for command.
+
+</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="92"/>
+        <source>%1 version %2
+</source>
+        <translation>%1 version %2
+</translation>
+    </message>
+</context>
+<context>
+    <name>Sudo</name>
+    <message>
+        <location filename="../sudo.cpp" line="195"/>
+        <source>%1: no command to run provided!</source>
+        <translation>%1: no command to run provided!</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="202"/>
+        <source>%1: no backend chosen!</source>
+        <translation>%1: no backend chosen!</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="219"/>
+        <source>Syscall error, failed to fork: %1</source>
+        <translation>Syscall error, failed to fork: %1</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="246"/>
+        <source>unset</source>
+        <extracomment>shouldn&apos;t be actually used but keep as short as possible in translations just in case.</extracomment>
+        <translation>unset</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="295"/>
+        <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
+</source>
+        <translation>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
+</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="337"/>
+        <source>Syscall error, failed to bring pty to non-block mode: %1</source>
+        <translation>Syscall error, failed to bring pty to non-block mode: %1</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="345"/>
+        <source>Syscall error, failed to fdopen pty: %1</source>
+        <translation>Syscall error, failed to fdopen pty: %1</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="314"/>
+        <source>%1: Failed to exec &apos;%2&apos;: %3
+</source>
+        <translation>%1: Failed to exec &apos;%2&apos;: %3
+</translation>
+    </message>
+    <message>
+        <location filename="../sudo.cpp" line="376"/>
+        <source>Child &apos;%1&apos; process failed!
+%2</source>
+        <translation>Child &apos;%1&apos; process failed!
+%2</translation>
+    </message>
+</context>
+</TS>
