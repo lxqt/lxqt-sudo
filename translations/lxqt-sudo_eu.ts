0d74e54ce8af110465ffd2ddd1be94de3da4f4b6
Weblate <noreply@weblate.org>
2025-07-07 19:32:17 +0200

    Weblate commits
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Roberalz <lumintnian@outlook.com>
    Co-authored-by: Simon Lindén <simon_l91@hotmail.com>
    Co-authored-by: Weblate <noreply@weblate.org>
    Co-authored-by: milotype <mail@milotype.de>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-sudo/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-sudo/hr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-configuration/lxqt-sudo/sv/
    Translation: LXQt Configuration/lxqt-sudo

--- translations/lxqt-sudo_eu.ts
+++ translations/lxqt-sudo_eu.ts
@@ -1,57 +1,57 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="hr">
+<TS version="2.1" language="eu">
 <context>
     <name>PasswordDialog</name>
     <message>
         <location filename="../passworddialog.ui" line="6"/>
         <source>LXQt sudo</source>
-        <translation>LXQt sudo</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="42"/>
         <source>Copy command to clipboard</source>
-        <translation>Kopiraj naredbu u međuspremnik</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="45"/>
         <source>&amp;Copy</source>
-        <translation>&amp;Kopiraj</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="83"/>
         <source>The requested action needs administrative privileges.&lt;br&gt;Please enter your password.</source>
-        <translation>Zatražena radnja zahtijeva administrativna prava.&lt;br&gt;Upiši svoju lozinku.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="106"/>
         <source>LXQt sudo backend</source>
-        <translation>Pozadinski sustav za LXQt sudo</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="109"/>
         <source>A program LXQt sudo calls in background to elevate privileges.</source>
-        <translation>Program koji LXQt sudo poziva u pozadini za mijenjanje korisničkih prava.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="119"/>
         <source>Command:</source>
-        <translation>Naredba:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="126"/>
         <source>Password:</source>
-        <translation>Lozinka:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.ui" line="133"/>
         <source>Enter password</source>
-        <translation>Upiši lozinku</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../passworddialog.cpp" line="60"/>
         <source>Attempt #%1</source>
-        <translation>%1. pokušaj</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -73,28 +73,13 @@ Arguments:
   arguments        Optional arguments for command.
 
 </source>
-        <translation>Upotreba: %1 opcija [naredba [argumenti ...]]
-
-GUI frontend for %2/%3/%4
-
-Argumenti:
-  opcije:
-    -h|--help      Ispiši ovu pomoć.
-    -v|--version   Ispiši podatke verzije.
-    -s|--su        Koristi %3(1) kao pozadinski sustav.
-    -d|--sudo      Koristi %2(8) kao pozadinski sustav.
-    -a|--doas      Koristi %4(1) kao pozadinski sustav.
-  command          Naredba koja se želi pokrenuti.
-  arguments        Opcionalni argumenti za naredbu.
-
-</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="96"/>
         <source>%1 version %2
 </source>
-        <translation>%1 verzija %2
-</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -102,54 +87,51 @@ Argumenti:
     <message>
         <location filename="../sudo.cpp" line="205"/>
         <source>%1: no command to run provided!</source>
-        <translation>%1: nijedna naredba za pokretanje nije zadana!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="212"/>
         <source>%1: no backend chosen!</source>
-        <translation>%1: nije odabran niti jedan pozadinski sustav!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="229"/>
         <source>Syscall error, failed to fork: %1</source>
-        <translation>Greška poziva sustava, neuspjelo kopiranje: %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="257"/>
         <source>unset</source>
         <extracomment>shouldn&apos;t be actually used but keep as short as possible in translations just in case.</extracomment>
-        <translation>nepostavljeno</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="323"/>
         <source>%1: Detected attempt to inject privileged command via LC_ALL env(%2). Exiting!
 </source>
-        <translation>%1: Otkriven je pokušaj dodavanja privilegirane naredbe putem LC_ALL env(% 2). Program se zatvara!
-</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="365"/>
         <source>Syscall error, failed to bring pty to non-block mode: %1</source>
-        <translation>Greška poziva sustava, neuspjelo postavljanje pty-a u neblokirajući način: %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="373"/>
         <source>Syscall error, failed to fdopen pty: %1</source>
-        <translation>Greška poziva sustava, neuspjelo fdopen pty: %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="342"/>
         <source>%1: Failed to exec &apos;%2&apos;: %3
 </source>
-        <translation>%1: Neuspjelo izvršavanje „%2”: %3
-</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../sudo.cpp" line="407"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
-        <translation>Neuspio podređeni „%1” proces!
-%2</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
